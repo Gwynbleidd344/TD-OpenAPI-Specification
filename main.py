76aedf78353c383032775c7ea163ae9e81c18e43
Vaniaina <hei.vaniaina@gmail.com>
2025-08-21 10:09:56 +0300

    ADD: EXO1

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
+from starlette.responses import Response
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/ping")
+async def pong():
+    return Response("pong", media_type="text/plain", status_code=200)
