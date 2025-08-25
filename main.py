a378d95f5c172e9d9b422e3e7cf3196eade77590
Vaniaina <hei.vaniaina@gmail.com>
2025-08-25 14:59:22 +0300

    fix: finished exo2 + removed server

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from starlette.responses import Response
-
-app = FastAPI()
-
-
-@app.get("/ping")
-async def pong():
-    return Response("pong", media_type="text/plain", status_code=200)
